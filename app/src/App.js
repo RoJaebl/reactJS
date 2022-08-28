286db1d5d58cd20e6991d3a4a570dbf57dd78ec2
Rojaebl <qgs0924@gmail.com>
2022-08-28 14:50:22 +0900

    :memo: useEffect added

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,18 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 import styles from "./App.module.css";
+
 function App() {
+  const [count, setCount] = useState(0);
+  const onClick = () => setCount((prev) => prev + 1);
+  console.log("i run all the time");
+  const iFunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(() => console.log("CALL THE API..."), []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!!</h1>
-      <Button pText={"Continue"} />
+      <h1 className={styles.title}>{count}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
