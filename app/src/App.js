4f2f13e249b24f240f8fdf8c16f4ffbe3f67a055
Rojaebl <qgs0924@gmail.com>
2022-08-28 15:18:20 +0900

    :memo: useEffect 'Deps' added study

--- app/src/App.js
+++ app/src/App.js
@@ -3,14 +3,30 @@ import styles from "./App.module.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setCount((prev) => prev + 1);
-  console.log("i run all the time");
-  const iFunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
-  useEffect(() => console.log("CALL THE API..."), []);
+  const onChange = (e) => setKeyword(e.target.value);
+
+  useEffect(() => console.log("I run only once."), []);
+  useEffect(() => {
+    console.log(`I run when 'keyword' changes.`);
+  }, [keyword]);
+  useEffect(() => {
+    console.log(`I run when 'count' changes.`);
+  }, [count]);
+  useEffect(() => {
+    console.log(`I run when keyword & count changes`);
+  }, [keyword, count]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>{count}</h1>
       <button onClick={onClick}>click me</button>
     </div>
