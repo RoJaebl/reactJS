f1954b557e3f452868a08a582e18c8f0a8553a7f
RoJaebl <qgs0924@gmail.com>
2022-08-31 00:18:48 +0900

    :hammer: Get USD to coin convert feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [conis, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [select, setSelect] = useState(0);
+  const onChange = (e) => {
+    setMoney(e.target.value);
+  };
+  const onChangeS = (e) => {
+    setSelect(e.target.value);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -10,17 +18,41 @@ function App() {
         setLoading(false);
       });
   }, []);
+
   return (
     <div>
-      <h1>The Coins! ({conis.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {conis.map((coin) => (
-          <li key={coin.id}>
-            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${conis.length})`} </h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onChangeS}>
+          {conis.map((coin) => (
+            <option key={coin.id} value={parseFloat(coin.quotes.USD.price)}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      {loading ? null : (
+        <div
+          style={{ display: "flex", alignItems: "center", flexWrap: "nowrap" }}
+        >
+          <h3 style={{ marginRight: "15px" }}>Your money(USD) </h3>
+          <input
+            value={money}
+            type="number"
+            onChange={onChange}
+            placeholder="your have money input"
+            style={{ marginRight: "15px" }}
+          />
+          <h3 style={{ marginRight: "15px" }}>Convert coins</h3>
+          <input
+            value={(money / select).toString()}
+            type="text"
+            disabled={true}
+          />
+        </div>
+      )}
     </div>
   );
 }
